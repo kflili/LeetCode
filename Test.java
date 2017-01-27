d66162c44206c40385d1a021f30bd531554ab50a
Li Li <kflili@gmail.com>
2017-01-26 23:02:16 -0500

    add Test template for testing code

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Test { 
+    public static void main(String[] args) {
+        // Add test code here
+
+        // Add any print statements here
+        System.out.print("Hello world!");  
+    }
+}
