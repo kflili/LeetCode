a7bab2de0c67e51c0f21390a5ffdbb901b3c0c16
Li Li <kflili@gmail.com>
2017-02-01 15:52:35 -0500

    add another solution

--- 7_ReverseInteger/Solution.java
+++ 7_ReverseInteger/Solution.java
@@ -13,7 +13,18 @@ public class Solution {
         }
         return result;
     }
-     
+    
+    // shorter solution
+    public int reverse2(int x) {
+        long rev= 0;
+        while( x != 0){
+            rev= rev*10 + x % 10;
+            x= x/10;
+            if( rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+                return 0;
+        }
+        return (int) rev;
+    }
     
     public static void main(String[] args) {
         int x = -123000;
